cdaeca8ba2cf408b916816e520c0e5db5189b8cc
bento <bento@licentia.pt>
2022-06-09 15:03:41 +0100

    s
    Signed-off-by: bento <bento@licentia.pt>

---
+++ Setup/Patch/Data/AddPandaInitialData.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Licentia\Panda\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+
+class AddPandaInitialData implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @var \Licentia\Panda\Helper\Data
+     */
+    protected $pandaHelper;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory          $eavSetupFactory
+     */
+    public function __construct(
+        \Licentia\Panda\Helper\Data $pandaHelper,
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+
+        $this->pandaHelper = $pandaHelper;
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $setup = $this->moduleDataSetup;
+        $connection = $setup->getConnection();
+
+        $now = $this->pandaHelper->gmtDate();
+
+        $template = '{{template config_path="design/email/header_template"}}
+                    
+{MESSAGE}
+                    
+{{template config_path="design/email/footer_template"}}';
+
+        $name = 'Default';
+        $total = $connection->fetchRow(
+            $connection->select()->from($setup->getTable('panda_templates_global'))
+        );
+
+        if (!$total) {
+            $connection
+                ->insert(
+                    $setup->getTable('panda_templates_global'),
+                    [
+                        'name'      => $name,
+                        'content'   => $template,
+                        'is_active' => 1,
+                    ]
+                );
+        }
+
+        $connection
+            ->insert(
+                $setup->getTable('cron_schedule'),
+                [
+                    'job_code'     => 'panda_build_metadata',
+                    'status'       => 'pending',
+                    'created_at'   => $now,
+                    'scheduled_at' => $now,
+                ]
+            );
+
+        $connection
+            ->insert(
+                $setup->getTable('cron_schedule'),
+                [
+                    'job_code'     => 'panda_rebuild_relations',
+                    'status'       => 'pending',
+                    'created_at'   => $now,
+                    'scheduled_at' => new \Zend_Db_Expr("DATE_ADD('$now',INTERVAL 24 HOUR)"),
+                ]
+            );
+
+        $connection
+            ->insert(
+                $setup->getTable('cron_schedule'),
+                [
+                    'job_code'     => 'panda_products_recommendations',
+                    'status'       => 'pending',
+                    'created_at'   => $now,
+                    'scheduled_at' => new \Zend_Db_Expr("DATE_ADD('$now',INTERVAL 24 HOUR)"),
+                ]
+            );
+
+        $connection
+            ->insert(
+                $setup->getTable('cron_schedule'),
+                [
+                    'job_code'     => 'panda_rebuild_products_performance',
+                    'status'       => 'pending',
+                    'created_at'   => $now,
+                    'scheduled_at' => new \Zend_Db_Expr("DATE_ADD('$now',INTERVAL 24 HOUR)"),
+                ]
+            );
+
+        $total = $connection->fetchRow(
+            $connection->select()->from($setup->getTable('panda_formulas'))
+        );
+
+        if (!$total) {
+            $connection->insert(
+                $setup->getTable('panda_formulas'),
+                [
+                    'formula_id'      => '1',
+                    'formula_1_name'  => 'Formula 1',
+                    'formula_2_name'  => 'Formula 2',
+                    'formula_3_name'  => 'Formula 3',
+                    'formula_4_name'  => 'Formula 4',
+                    'formula_5_name'  => 'Formula 5',
+                    'formula_6_name'  => 'Formula 6',
+                    'formula_7_name'  => 'Formula 7',
+                    'formula_8_name'  => 'Formula 8',
+                    'formula_9_name'  => 'Formula 9',
+                    'formula_10_name' => 'Formula 10',
+                ]
+            );
+        }
+        $indexCampaign = $connection->fetchOne(
+            "SHOW INDEX FROM `{$setup->getTable('sales_order')}` WHERE Key_name = 'SALES_ORDER_ACQUISITION_CAMPAIGN'  "
+        );
+
+        if (!$indexCampaign) {
+            $setup->run(
+                "ALTER TABLE `{$setup->getTable('sales_order')}` ADD INDEX `SALES_ORDER_ACQUISITION_CAMPAIGN` USING BTREE (`panda_acquisition_campaign`) "
+            );
+        }
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+
+        return [
+
+        ];
+    }
+}
---
+++ Setup/Patch/Data/AddPandaSegmentsProductAttribute.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Licentia\Panda\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+
+class AddPandaSegmentsProductAttribute implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory          $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'panda_segments',
+            [
+                'type'                    => 'varchar',
+                'label'                   => 'Panda Segments',
+                'input'                   => 'multiselect',
+                'frontend'                => '',
+                'required'                => false,
+                'backend'                 => 'Licentia\Equity\Model\Products\Attribute\Backend\SegmentsPrices',
+                'sort_order'              => '30',
+                'global'                  => ScopedAttributeInterface::SCOPE_WEBSITE,
+                'default'                 => null,
+                'visible'                 => true,
+                'user_defined'            => true,
+                'searchable'              => false,
+                'filterable'              => false,
+                'comparable'              => false,
+                'visible_on_front'        => false,
+                'unique'                  => false,
+                'apply_to'                => 'simple',
+                'group'                   => 'General',
+                'used_in_product_listing' => false,
+                'is_used_in_grid'         => true,
+                'is_visible_in_grid'      => false,
+                'is_filterable_in_grid'   => false,
+                'option'                  => '',
+            ]
+        );
+        $entityTypeId = $eavSetup->getEntityTypeId(\Magento\Catalog\Model\Product::ENTITY);
+        $attributeSetId = $eavSetup->getDefaultAttributeSetId($entityTypeId);
+
+        $groups['Advanced Pricing']['id'] = $eavSetup->getAttributeGroupId(
+            $entityTypeId,
+            $attributeSetId,
+            'Advanced Pricing'
+        );
+
+        // update attributes group and sort
+        $attributes = [
+            'panda_segments' => ['group' => 'Advanced Pricing', 'sort' => 100],
+        ];
+
+        foreach ($attributes as $attributeCode => $attributeProp) {
+            $eavSetup->addAttributeToGroup(
+                $entityTypeId,
+                $attributeSetId,
+                $groups[$attributeProp['group']]['id'],
+                $attributeCode,
+                $attributeProp['sort']
+            );
+        }
+        
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->removeAttribute(\Magento\Catalog\Model\Product::ENTITY, 'panda_segments');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+
+        return [
+
+        ];
+    }
+}
